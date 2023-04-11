53feb2a39cbd8b4c6947628f76c293fa2b464fbf
Vlase Rafaella <kmyelly2@gmail.com>
2023-04-11 14:39:26 +0300

    add view and controller classes

--- PT2023_30225_Vlase_Rafaella_Assignment_2/src/main/java/org/example/Main.java
+++ PT2023_30225_Vlase_Rafaella_Assignment_2/src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
-
+import javax.swing.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //new View("Queue Application");
     }
 }
