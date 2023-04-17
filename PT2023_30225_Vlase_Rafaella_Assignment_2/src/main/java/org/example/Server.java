e538c8ae787216f66016a2979b0a9bdbeaaee595
Vlase Rafaella <kmyelly2@gmail.com>
2023-04-17 15:04:31 +0300

    add classes

---
+++ PT2023_30225_Vlase_Rafaella_Assignment_2/src/main/java/org/example/Server.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Server implements Runnable
+{
+    public void run()
+    {
+        while (true)
+        {
+
+        }
+    }
+}
---
+++ PT2023_30225_Vlase_Rafaella_Assignment_2/src/main/java/org/example/SimulationManager.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class SimulationManager implements Runnable
+{
+
+}
